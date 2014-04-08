6996f24b4baaec127eb1e174a76f019b9287bd3a
Hannyajin <talmo.christian@gmail.com>
2014-04-08 19:33:46 +0300

    basic ballistics test

---
+++ utils.js
@@ -0,0 +1,49 @@
+/**
+  * Utils
+  */
+
+var Vector2D = (function() {
+  var self = this;
+
+  self.add = function(v1, v2) {
+    var x = v1.x + v2.x;
+    var y = v1.y + v2.y;
+    return {x: x, y: y};
+  };
+
+  self.subtract = function(v1, v2) {
+    var x = v1.x - v2.x;
+    var y = v1.y - v2.y;
+    return {x: x, y: y};
+  };
+
+  self.length = function(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+  };
+
+  self.scale = function(v, scale) {
+    return {x: v.x * scale, y: v.y * scale};
+  };
+
+  self.distance = function(v1, v2) {
+    var dx = v1.x - v2.x;
+    var dy = v1.y - v2.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  self.dotProduct = function(v1, v2) {
+    var dp = v1.x * v2.x + v1.y * v2.y;
+    return dp;
+  };
+
+  self.normalize = function(v) {
+    var leng = Math.sqrt(v.x * v.x + v.y * v.y)
+    if (leng > 0) {
+      return { x: v.x / leng, y: v.y / leng };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  };
+
+  return self;
+})();
