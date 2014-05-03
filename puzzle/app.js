d355090706bf57b913ba4237ea88586ac951be06
Hannyajin <talmo.christian@gmail.com>
2014-05-03 09:49:34 +0300

    sliced img to grids

--- puzzle/app.js
+++ puzzle/app.js
@@ -12,9 +12,55 @@ img.src = "aphextwin.jpg"
 var ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "rgb(200,0,0)";  
-ctx.fillRect(10, 10, 55, 50); 
+ctx.fillText("Loading", 25, 25);
+
+/*
+var c = createjs;
+var stage = new c.Stage(canvas);
+*/
+
+var gridw = 3;
+var gridh = 4; // 3 by 4 = 12 pieces
+
+// 2d array
+var grid = (function() {
+	var arr = new Array(gridw);
+	for (var i = 0; i < gridw; i++) {
+		arr[i] = new Array(gridh);
+	}
+	return arr;
+})();
+
+var Piece = function(sx,sy,sw,sh,x,y,w,h) {
+	this.sx = sx;
+	this.sy = sy;
+	this.sw = sw;
+	this.sh = sh;
+
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+
+	this.draw = function(ctx) {
+		ctx.drawImage(img, this.sx, this.sy, this.sw, this.sh, this.x, this.y, this.w, this.h);
+	}
+}
 
 img.onload = function() {
-	ctx.drawImage(img, 0, 0);
+	for (var i = 0; i < gridw; i++) {
+		for (var j = 0; j < gridh; j++) {
+			var w = img.width / gridw;
+			var h = img.height / gridh;
+			var x = i * w;
+			var y = j * h;
+			var dx = canvas.width / gridw * i;
+			var dy = canvas.height / gridh * j
+			var p = new Piece(x, y, x + w, y + h,
+				dx, dy,
+				dx + canvas.width / gridw, dy + canvas.height / gridh);
+			grid[i][j] = p;
+			p.draw(ctx);
+		}
+	}
 }
-//ctx.drawImage(100, 100, image);
